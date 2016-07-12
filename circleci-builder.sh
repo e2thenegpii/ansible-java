a6bdb293d85858ed92d4264c30d369be084f1a5f
Andrew Rothstein <andrew.rothstein@gmail.com>
2016-07-11 22:41:22 -0400

    adjust tested platforms

--- circleci-builder.sh
+++ circleci-builder.sh
@@ -5,7 +5,7 @@ TARGET_ROLE=$1
 
 i=0
 files=()
-for os in centos_7 fedora_22 fedora_23 ubuntu_trusty ubuntu_vivid ubuntu_wily ubuntu_xenial; do
+for os in centos_7 fedora_23 ubuntu_trusty ubuntu_xenial alpine_3.3 alpine_3.4 alpine_edge; do
     if [ $(($i % $CIRCLE_NODE_TOTAL)) -eq $CIRCLE_NODE_INDEX ]
     then
 	docker pull andrewrothstein/docker-ansible-role:$os
